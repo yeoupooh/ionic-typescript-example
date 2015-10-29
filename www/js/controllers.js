f6eb9da387de1aba2ab144fcd8fa80bcd2335f0c
mhartington <mikehartington@gmail.com>
2015-10-29 12:25:49 -0400

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+/// <reference path="../typings/tsd.d.ts" />
+angular.module('starter.controllers', [])
+    .controller('DashCtrl', function ($scope, $ionicLoading) {
+})
+    .controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
+})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+    .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
